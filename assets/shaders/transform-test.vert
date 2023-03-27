2474145b25ca2d8742c5062492e4fd984a49e99d
Nour Almulhem <nouralmulhem@gmail.com>
2023-03-27 23:19:19 +0200

    transformation

--- assets/shaders/transform-test.vert
+++ assets/shaders/transform-test.vert
@@ -16,7 +16,9 @@ uniform mat4 transform;
 
 void main(){
     //TODO: (Req 3) Change the next line to apply the transformation matrix
-    
+    gl_Position = transform * vec4(position, 1.0);
+    // multiply the transformation matrix by the position vector
+
     // No need to change any of the following lines
     vs_out.position = position;
     vs_out.color = color;
--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -3,6 +3,7 @@
 
 #include <glm/gtx/euler_angles.hpp>
 
+
 namespace our {
 
     // This function computes and returns a matrix that represents this transform
@@ -10,7 +11,19 @@ namespace our {
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
     glm::mat4 Transform::toMat4() const {
         //TODO: (Req 3) Write this function
-        return glm::mat4(1.0f); 
+        glm::mat4 translate = glm::translate(glm::mat4(1.0f), position);
+        // here we create the translate matrix given the position
+        glm::mat4 scaling = glm::scale(glm::mat4(1.0f), scale);
+        // here we create the scale matrix
+        glm::mat4 rotate = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
+        // here we create the rotation matrix
+        
+        glm::mat4 transform = translate * rotate * scaling;
+        // we got the transform out of multiplication of the three matrices TRS
+        // in the following order so that the vector is got scaled first 
+        // then rotated and finally translated to the position
+
+        return transform; 
     }
 
      // Deserializes the entity data and components from a json object

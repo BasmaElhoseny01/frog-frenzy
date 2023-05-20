e1576c7a35119d7fff65a43458165b86afb89b5f
AhmedHosny2024 <eng.ahmedhosny2024@gmail.com>
2023-05-20 23:10:24 +0300

    add light to car-taxi-frog

--- source/states/play-state.hpp
+++ source/states/play-state.hpp
@@ -113,8 +113,8 @@ class Playstate : public our::State
         carsSystem.update(&world); // To control Cars System to appear
         collisionSystem.update(&world,&renderer,flagPostProcessing,engine,id);// To check collision
         movementSystem.update(&world, (float)deltaTime); // To update movement component 
-        cameraController.update(&world, (float)deltaTime);
-        // frogController.update(&world, (float)deltaTime);// To control frog movement
+        // cameraController.update(&world, (float)deltaTime);
+        frogController.update(&world, (float)deltaTime);// To control frog movement
         gainHeartSystem.update(&world,id);// To check Gaining new Heart
 
         // Remove Marked for removal Entities[Basma] so that they aren't rendered again
